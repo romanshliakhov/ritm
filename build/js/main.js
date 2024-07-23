f4af004803b10307a334959f2b5a1f153eedc84c
admin <admin@admins-MacBook-Pro.local>
2024-07-23 18:02:13 +0300

    add egistracii-dilerov pages

--- build/js/main.js
+++ build/js/main.js
@@ -183,24 +183,26 @@ const fieldOrg = document.querySelector('.js-form-item-field-nameorganiz');
 const activeAdress = document.querySelector('.js-form-item-field-factadres');
 const contactFace = document.querySelector('.js-form-item-field-kontaktlico');
 const fieldFio = document.querySelector('.js-form-item-field-fio');
-function updateFormVisibility() {
-  if (fizicheskoe.checked) {
-    fieldFio.style.display = 'block';
-    fieldOrg.style.display = 'none';
-    activeAdress.style.display = 'none';
-    contactFace.style.display = 'none';
-  } else if (yuredicheskoe.checked) {
-    fieldFio.style.display = 'none';
-    fieldOrg.style.display = 'block';
-    activeAdress.style.display = 'block';
-    contactFace.style.display = 'block';
+if (fizicheskoe && yuredicheskoe) {
+  function updateFormVisibility() {
+    if (fizicheskoe.checked) {
+      fieldFio.style.display = 'block';
+      fieldOrg.style.display = 'none';
+      activeAdress.style.display = 'none';
+      contactFace.style.display = 'none';
+    } else if (yuredicheskoe.checked) {
+      fieldFio.style.display = 'none';
+      fieldOrg.style.display = 'block';
+      activeAdress.style.display = 'block';
+      contactFace.style.display = 'block';
+    }
   }
-}
-fizicheskoe.addEventListener('change', updateFormVisibility);
-yuredicheskoe.addEventListener('change', updateFormVisibility);
+  fizicheskoe.addEventListener('change', updateFormVisibility);
+  yuredicheskoe.addEventListener('change', updateFormVisibility);
 
-// Обновление формы при загрузке страницы
-document.addEventListener('DOMContentLoaded', updateFormVisibility);
+  // Обновление формы при загрузке страницы
+  document.addEventListener('DOMContentLoaded', updateFormVisibility);
+}
 
 /***/ }),
 
