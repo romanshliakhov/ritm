5b9fae1ee984944672f193473088bcf984f20db3
admin <admin@admins-MacBook-Pro.local>
2024-07-28 14:45:02 +0300

    ssc section

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,17 +1,7 @@
 import './components/sliders';
+import './components/formFieldLico';
 import './components/burger';
 import './components/getDinamicHeight';
-import './components/tabs';
-// import './components/accordions';
-// import './components/replaceEl';
-// import './components/modals';
-// import './components/aos';
-import './components/select';
-import './components/formFieldLico';
-// import './components/counter.js';
-// import './components/inputMask.js';
-// import './components/hiddenCards';
-// import './components/form';
 
 
 
