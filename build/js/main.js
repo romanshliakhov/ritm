7c7f9f1fdb4f901b80d33e3ea90c274f7bae0e55
Roman <romanshliakhov@gmail.com>
2024-10-15 21:03:22 +0300

    restore promo block cover fit

--- build/js/main.js
+++ build/js/main.js
@@ -287,11 +287,10 @@ if (promoSlider) {
     navigation: {
       nextEl: '.promo-next',
       prevEl: '.promo-prev'
+    },
+    autoplay: {
+      delay: 3000
     }
-
-    // autoplay: {
-    //   delay: 3000,
-    // },
   });
 }
 
